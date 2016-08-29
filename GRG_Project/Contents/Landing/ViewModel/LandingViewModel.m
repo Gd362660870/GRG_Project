35707f62e5806d6fc1f7a6d36111dadaa6e636ad
Gd362660870 <JJ@lalalala.local>
2016-08-29 11:28:12 +0800

    aajajjajaja
    adfsafdsafdas

---
+++ GRG_Project/Contents/Landing/ViewModel/LandingViewModel.m
@@ -0,0 +1,13 @@
+//
+//  LandingViewModel.m
+//  GRG_Project
+//
+//  Created by 陈家劲 on 16/8/29.
+//  Copyright © 2016年 陈家劲. All rights reserved.
+//
+
+#import "LandingViewModel.h"
+
+@implementation LandingViewModel
+
+@end
