22124eede7b95a045f8d9fd1a112dac03aca0a21
Gd362660870 <JJ@lalalala.local>
2016-08-29 10:55:54 +0800

    test2
    test—02

---
+++ GRG_Project/Contents/Landing/Model/LandingModel.h
@@ -0,0 +1,13 @@
+//
+//  LandingModel.h
+//  GRG_Project
+//
+//  Created by 陈家劲 on 16/8/29.
+//  Copyright © 2016年 陈家劲. All rights reserved.
+//
+
+#import "SuperModel.h"
+
+@interface LandingModel : SuperModel
+
+@end
